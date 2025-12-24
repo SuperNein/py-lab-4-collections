9c68861f613b9e86fc66ece22087fb5a4ac2fb99
Konstantin Smage <kpsmage@mai.education>
2025-12-24 07:25:29 +0300

    feat: add simulation
    - Add logging
    - Add main

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
+import logging
+
+from src.common.config import LOGGING_CONFIG
+from src.services.simulation import Simulation
+
+
 def main() -> None:
     """
     Main entry point for the application.
     :return:   None
     """
-    pass
+    logging.config.dictConfig(LOGGING_CONFIG)
+    logger = logging.getLogger(__name__)
+
+    print("Welcome to the Library!")
+    seed = input("Enter seed: ")
+    if seed.isnumeric():
+        seed = int(seed)
+    else:
+        seed = None
+    simulation = Simulation(logger, seed)
+
+    steps = int(input("Enter number of steps: "))
+    simulation.run(steps)
 
 
 if __name__ == "__main__":
